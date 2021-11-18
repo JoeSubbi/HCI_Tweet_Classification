53b77dd6106d1845799e92a065ca67958f6c3037
Owen Shade <2378587s@student.gla.ac.uk>
2021-11-18 15:44:48 +0000

    created login/create account/log out functionality

--- tweets_on_trial/app/urls.py
+++ tweets_on_trial/app/urls.py
@@ -6,4 +6,7 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('judge/', views.judge, name='judge'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout',)
 ]
--- tweets_on_trial/app/views.py
+++ tweets_on_trial/app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from app.forms import UserForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
@@ -12,3 +16,45 @@ def index(request):
 
 def judge(request):
     return HttpResponse("JUDGE")
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+
+        if user_form.is_valid:
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+    return render(request, 'app/register.html', context={'user_form': user_form, 'registered': registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('app:index'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print(f"invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'app/login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+
+    return redirect(reverse('app:index'))
